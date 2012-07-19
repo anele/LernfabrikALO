c4900ca58e2fd5a77f1274b49375ede66f47a549
Lena Lorenz <helorenz@htwg-konstanz.de>
2012-07-19 16:21:25 +0200

    commit changes

--- LernfabrikALO_sdk/src/com/example/alo/MainActivity.java
+++ LernfabrikALO_sdk/src/com/example/alo/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends Activity {
 	
 	
 	@Override
+	//Called then the activity is first created
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
